61e0f3129540b57a496e52327023f7cdb7bbc976
ConorOGorman <ogormanconor05@gmail.com>
2025-11-30 16:40:07 +0100

    Use React components for Vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import LandingPagePartsHub from './pages/LandingPagePartsHub'
+import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 
 export default function App(): JSX.Element {
   return (
     <>
       <LandingPagePartsHub />
+      <Analytics />
       <SpeedInsights />
     </>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles/index.css'
-import { inject } from '@vercel/analytics'
-
-inject()
 
 const root = createRoot(document.getElementById('root')!)
 root.render(
