e2599f301b84591e5621fae7078483ba91413dc1
ConorOGorman <ogormanconor05@gmail.com>
2025-11-30 15:39:07 +0100

    Add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles/index.css'
+import { inject } from '@vercel/analytics'
+
+inject()
 
 const root = createRoot(document.getElementById('root')!)
 root.render(
