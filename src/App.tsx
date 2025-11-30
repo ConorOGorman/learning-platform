37c8a4cc31cdb9c2bac9c6bdb0ed466c993240e5
ConorOGorman <ogormanconor05@gmail.com>
2025-11-30 15:43:05 +0100

    Add Vercel Analytics and Speed Insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import LandingPagePartsHub from './pages/LandingPagePartsHub'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 export default function App(): JSX.Element {
-  return <LandingPagePartsHub />
+  return (
+    <>
+      <LandingPagePartsHub />
+      <SpeedInsights />
+    </>
+  )
 }
